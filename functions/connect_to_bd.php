71b9b6d0dbd3492c525a26d8204631f86898cbea
ekanga <ekangay18@gmail.com>
2025-05-05 02:36:19 +0200

    second commit

--- functions/connect_to_bd.php
+++ functions/connect_to_bd.php
@@ -2,16 +2,15 @@
 
 session_start();
 
-$servername= "mysql-yanndev.alwaysdata.net";
-$dbname= "yanndev_eya";
-$username= "yanndev";
-$password= "yanndev-bot";
+$servername= "localhost";
+$dbname= "blog";
+$username= "root";
+$password= "";
 
 try {
     $conn=new PDO("mysql:host=$servername;dbname=$dbname; $username,$password");
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo $e->getMessage();
-   die('Une erreur est survenue lors de la connexion a la base de donnée');
+    echo'Une erreur est survenue lors de la connexion a la base de donnée'. $e->getMessage();
 }
 ?>
